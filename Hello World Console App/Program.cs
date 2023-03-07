49c62f33baf193631cceeb2ddf7d38ff8aa435db
Petya Vegenova <Petya.Vegenova@iris.co.uk>
2023-03-07 14:01:39 +0200

    Create Hello World Console App

---
+++ Hello World Console App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hello_World_Console_App
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
